ad1788d2087a9b8662c4cb56bf0124b47893a5c6
Miley Hollenberg <mileyhollenberg@gmail.com>
2020-05-30 13:58:14 +0300

    Initial commit

---
+++ Editor/ChangeCameraAimOnStart.cs
@@ -0,0 +1,109 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityToolbarExtender;
+
+namespace MegaTools.Editor.Toolbar
+{
+    internal static class CameraAimToolbarStyles
+    {
+        public static readonly GUIStyle commandButtonStyleEnabled;
+        public static readonly GUIStyle commandButtonStyleDisabled;
+
+        private const int _buttonWidth = 150;
+
+        static CameraAimToolbarStyles()
+        {
+            commandButtonStyleEnabled = new GUIStyle("Command")
+            {
+                fontSize = GUI.skin.label.fontSize,
+                alignment = TextAnchor.MiddleCenter,
+                imagePosition = ImagePosition.ImageAbove,
+                fontStyle = FontStyle.Bold,
+                normal = new GUIStyleState
+                {
+                    textColor = GUI.skin.label.normal.textColor,
+                },
+                fixedWidth = _buttonWidth,
+            };
+
+            commandButtonStyleDisabled = new GUIStyle("Command")
+            {
+                fontSize = GUI.skin.label.fontSize,
+                alignment = TextAnchor.MiddleCenter,
+                imagePosition = ImagePosition.ImageAbove,
+                fontStyle = FontStyle.Normal,
+                normal = new GUIStyleState
+                {
+                    textColor = GUI.skin.label.normal.textColor,
+                },
+                fixedWidth = _buttonWidth,
+            };
+        }
+    }
+
+    [InitializeOnLoad]
+    public class ChangeCameraAimOnStart
+    {
+        private static Vector3 _previousPosition;
+        private static Quaternion _previousRotation;
+
+        private static bool IsEnabled { get; set; }
+
+        static ChangeCameraAimOnStart()
+        {
+            ToolbarExtender.LeftToolbarGUI.Add(OnToolbarGUI);
+            IsEnabled = EditorPrefs.GetBool("FocusCamera_IsEnabled");
+            EditorApplication.playModeStateChanged += EditorApplicationOnplayModeStateChanged;
+        }
+
+        private static void EditorApplicationOnplayModeStateChanged(PlayModeStateChange obj)
+        {
+            if (Camera.main == null)
+            {
+                Debug.LogError("No main camera found");
+                return;
+            }
+
+            Transform camTransform = Camera.main.transform;
+
+            if (IsEnabled && obj == PlayModeStateChange.ExitingEditMode)
+            {
+                Transform sceneCamTransform = SceneView.lastActiveSceneView.camera.transform;
+
+                _previousPosition = camTransform.position;
+                _previousRotation = camTransform.rotation;
+
+                camTransform.position = sceneCamTransform.position;
+                camTransform.rotation = sceneCamTransform.rotation;
+            }
+            else if (IsEnabled && obj == PlayModeStateChange.EnteredEditMode)
+            {
+                camTransform.position = _previousPosition;
+                camTransform.rotation = _previousRotation;
+            }
+        }
+
+        private static void OnToolbarGUI()
+        {
+            GUILayout.FlexibleSpace();
+
+            string focusText = IsEnabled ? "Start at scene position" : "Start at camera position";
+            GUIStyle style = IsEnabled
+                ? CameraAimToolbarStyles.commandButtonStyleEnabled
+                : CameraAimToolbarStyles.commandButtonStyleDisabled;
+            const string tooltip =
+                "Start your project at the current scene view position or keep the camera object position";
+            
+            if (GUILayout.Button(new GUIContent(focusText, tooltip), style))
+            {
+                SetEnabled(!IsEnabled);
+            }
+        }
+
+        private static void SetEnabled(bool enabled)
+        {
+            IsEnabled = enabled;
+            EditorPrefs.SetBool("FocusCamera_IsEnabled", IsEnabled);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # ChangeCameraAimOnStart
 Ever wanted to start your game from the scene views position instead of the camera position? Look no further!
+
+# Example
+
+[<img src="https://img.youtube.com/vi/steO_QuEASM/maxresdefault.jpg" width="50%">](https://youtu.be/steO_QuEASM)
+
+# Setup
+
+* Install https://github.com/marijnz/unity-toolbar-extender through [github directly into your project](https://docs.unity3d.com/Manual/upm-ui-giturl.html)
+* Add `git@github.com:MileyHollenberg/ChangeCameraAimOnStart.git` through the same method as well
+
+# Tested version
+
+This extension has only been tested with 2019.3 so far but should work just fine with 2019.X and 2020.X. If you run into any problems please open an issue.
